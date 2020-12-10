1de90ebf40e715b7d6208bc68ee76aa060066b27
Eamon Niknafs <en@csa2.bu.edu>
2020-12-10 07:31:42 -0500

    Added *Stack_create()  - creates a new stack  that points to newly allocated memory of the size of the Stack struct  - sets top to -1  - allocates memory for the items, of size capacity * 8 bytes  - returns the stack

--- stackADT4.c
+++ stackADT4.c
@@ -14,6 +14,13 @@
 /* Stack structure */
 struct Stack {
     int top;
-    unsigned int capacity;
-    int* array;
+    unsigned int capacity;    int *items;
 };
+
+struct Stack *Stack_create(unsigned int capacity){
+    struct Stack *stack = (struct Stack*)malloc(sizeof(struct Stack));
+    stack->capacity = capacity;
+    stack->top = -1;
+    stack->items = (int*)malloc(stack->capacity * sizeof(int));
+    return stack;
+}
