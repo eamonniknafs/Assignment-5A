d4877715389c5bd08d87bbe5468f5fe7e8994c98
Eamon Niknafs <en@csa2.bu.edu>
2020-12-10 08:02:50 -0500

    Starting over

--- stackADT4.c
+++ stackADT4.c
@@ -12,25 +12,9 @@
 #include <stdlib.h>
 
 /* Stack structure */
-struct Stack {
+typedef struct{
     int top;
-    unsigned int capacity;    int *items;
-};
+    unsigned int size;
+    void *data;
+} Stack;
 
-struct Stack *Stack_create(unsigned int capacity){
-    struct Stack *stack = (struct Stack*)malloc(sizeof(struct Stack));
-    stack->capacity = capacity;
-    stack->top = -1;
-    stack->items = (int*)malloc(stack->capacity * sizeof(int));
-    return stack;
-}
-
-bool Stack_is_empty(struct Stack* stack)
-{
-    return stack->top == -1;
-}
-
-bool Stack_is_full(struct Stack* stack)
-{
-    return stack->top == stack->capacity - 1;
-}
