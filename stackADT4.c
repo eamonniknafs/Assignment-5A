90260529b1dcb7e0d9235880cb7b3c9b0ebed7e8
Eamon Niknafs <en@csa2.bu.edu>
2020-12-10 08:21:35 -0500

    Remade stack struct and Stack_create()

--- stackADT4.c
+++ stackADT4.c
@@ -7,14 +7,22 @@
 #endif
 #include "memtrace.h"
 
-#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "stackADT.h"
 
-/* Stack structure */
-typedef struct{
-    int top;
-    unsigned int size;
-    void *data;
-} Stack;
+#define SIZE 50
 
+struct stack_type {
+  int items[SIZE];
+  int top;
+};
+
+Stack Stack_create(void)
+{
+  Stack stack = malloc(sizeof(struct stack_type));
+  if (stack == NULL)
+    exit(1);
+  stack->top = 0;
+  return s;
+}
